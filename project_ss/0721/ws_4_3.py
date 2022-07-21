03d183d395daca68328ac1b28bed04f54fe019e0
sshh121 <lc307715@gmail.com>
2022-07-21 23:07:42 +0900

    0720 | project_home

--- project_ss/0721/ws_4_3.py
+++ project_ss/0721/ws_4_3.py
@@ -1,7 +1,12 @@
-'''
-'''
 ####미완 ㅠ
-numbers = list(input())
+numbers = input()
+
+
+#[1,1,3,3,0,1,1]
+
+
+
+'''
 len_num = len(numbers)
 for num in range(len_num-1):
     if numbers[num] == numbers[num+1] :
@@ -9,3 +14,12 @@ for num in range(len_num-1):
         len_num -=1
         
 print(numbers)
+
+
+for i, num in enumerate(numbers):
+    print(i, num)
+    # if numbers[i]
+print(numbers)
+
+
+'''
