0f35f18c81e1d15f92ed4311b6ac2b139f942039
sshh121 <lc307715@gmail.com>
2022-07-27 23:59:27 +0900

    0727 | Project_ss

---
+++ project_ss/0727/hw_7_4.py
@@ -0,0 +1,13 @@
+def collatz(num):
+    count = 0
+    while num > 1:
+        if num%2:
+            num = num*3+1
+        else:
+            num = num//2
+        count += 1
+        if count >= 500:
+            return -1
+    return count
+
+print(collatz(6))
