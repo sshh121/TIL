fd9326d75a46e62067a63e7e2cc61dca3dadea7e
sshh121 <lc307715@gmail.com>
2022-08-07 22:41:58 +0900

    del

--- project_ss/01_python/0718/ws_1_2.py
+++
@@ -1,4 +0,0 @@
-board_total = input('게시글의 총 갯수를 입력하세요 : ')
-board = input('한 페이지에 필요한 게시글 수를 입력하세요. : ')
-
-print(int(board_total)//int(board))
--- project_ss/01_python/additional/hw_4_4.py
+++
@@ -1,8 +0,0 @@
-words = ['eat', 'tea', 'tan', 'ate', 'nat', 'bat']
-
-for idx in range(len(words)):
-    words[idx] = set(words[idx])
-print(words)
-
-for word in words:
-    word & words[:]
